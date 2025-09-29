4814597ca4e5707240e85f81ea69737154b09307
Michal Kopecký <kpc@email.cz>
2025-09-29 08:53:23 +0200

    Change dir structure

--- clear.bat
+++ clear.bat
@@ -1,3 +1,3 @@
-call rmdir CryptoMonitor\obj /s /q
-call rmdir CryptoMonitor\bin /s /q
-call rmdir .vs /s /q
+call rmdir src\CryptoMonitor\obj /s /q
+call rmdir src\CryptoMonitor\bin /s /q
+call rmdir src\.vs /s /q
